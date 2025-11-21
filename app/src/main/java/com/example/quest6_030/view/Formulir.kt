0332e16c38b312c2b8a659be70607968b1397f6e
gagatpadi <padiyogaradhana@gmail.com>
2025-11-21 16:15:25 +0700

    terbaru

--- app/src/main/java/com/example/quest6_030/view/Formulir.kt
+++ app/src/main/java/com/example/quest6_030/view/Formulir.kt
@@ -1,17 +1,21 @@
 package com.example.quest6_030.view
 
+import android.annotation.SuppressLint
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.selection.selectable
 import androidx.compose.material3.Button
 import androidx.compose.material3.DividerDefaults.Thickness
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.HorizontalDivider
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.RadioButton
 import androidx.compose.material3.Scaffold
@@ -19,11 +23,15 @@ import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateListOf
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.Outline
-import androidx.compose.ui.layout.HorizontalAlignmentLine
 import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
@@ -32,38 +40,57 @@ import com.example.quest6_030.R
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun FormIsian(
-    pilihanJK: List<String>
+
+    pilihanJK: List<String> ,
+    onSubmitButtonClicked: (MutableList<String>) -> Unit,
+    modifier: Modifier = Modifier
 ){
+
+    var txtNama by rememberSaveable { mutableStateOf("") }
+    var txtAlamat by remember { mutableStateOf("") }
+    var txtGender by remember { mutableStateOf("") }
+    val lisData: MutableList<String> = mutableStateOf(txtNama, txtGender, txtAlamat)
+
     Scaffold (modifier = Modifier,
-        topBar = {
+        {
             TopAppBar(
-                title = {Text(text = stringResource(id= R.string.home), color = Color.White)},
+                title = { Text(text = stringResource(id= R.string.home), color = Color.White)},
                 colors = TopAppBarDefaults.mediumTopAppBarColors(
                     containerColor = colorResource(id = R.color.teal_700))
             ) }
-    ){ isiRuang ->
-        Column(modifier = Modifier.padding(paddingValues = isiRuang),
+    ) { isiRuang ->
+        Column (modifier = Modifier.padding(paddingValues = isiRuang),
             verticalArrangement = Arrangement.Top,
             horizontalAlignment = Alignment.CenterHorizontally) {
             OutlinedTextField(
-                value = "",
+                value =txtNama,
                 singleLine = true,
                 modifier = Modifier
                     .padding(top = 20.dp)
                     .width(width = 250.dp),
                 label = {Text(text = "Nama Lengkap")},
-                onValueChange = {}
+                onValueChange = {
+                    txtNama = it
+                },
             )
             HorizontalDivider(modifier = Modifier
                 .padding(all = 20.dp)
                 .width(width = 250.dp), thickness = Thickness,color = Color.Red)
-            Row{
-                jenisK.forEach {
-                        item->
-                    Row (verticalAlignment = Alignment.CenterVertically){
+
+            Row {
+                pilihanJK.forEach { item ->
+                    Row(modifier = Modifier.selectable(
+                        selected = txtGender == item,
+                        onClick = {
+                            txtGender = item
+                        }
+                    ),
+                        verticalAlignment = Alignment.CenterVertically) {
                         RadioButton(
-                            selected = false,
-                            onClick = {item}
+                            selected = txtGender == item,
+                            onClick = {
+                                txtGender = item
+                            }
                         )
                     }
                 }
@@ -75,22 +102,24 @@ fun FormIsian(
                 color = Color.Red
             )
             OutlinedTextField(
-                value = "",
+                value = txtAlamat,
                 singleLine = true,
+                shape = MaterialTheme.shapes.medium,
                 modifier = Modifier
                     .width(width = 250.dp),
-                label = {Text(text = "Alamat")},
-                onValueChange = {},
+                label = {Text(text = "Alamat Lengkap")},
+                onValueChange = {
+                    txtAlamat = it
+                }
             )
             Spacer(modifier = Modifier.height(height = 30.dp))
             Button(
-                modifier = Modifier.fillMaxWidth(fraction = 1f)
-                    .padding(all = 25.dp),
-                onClick = OnSubmitBtnClick
+                modifier = Modifier.fillMaxWidth(fraction = 1f),
+                enabled = txtAlamat.isNotEmpty(),
+                onClick = {onSubmitButtonClicked(lisData)}
             ) {
                 Text(text = stringResource(id = R.string.submit))
             }
         }
     }
-
 }
